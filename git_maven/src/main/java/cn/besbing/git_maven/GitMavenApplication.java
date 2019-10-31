497756d4aea2be23b141066ec32f28846caedd2f
sheny <fsbydz@vip.qq.com>
2019-10-31 08:52:19 +0800

    first message for git ---sheny

---
+++ git_maven/src/main/java/cn/besbing/git_maven/GitMavenApplication.java
@@ -0,0 +1,13 @@
+package cn.besbing.git_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMavenApplication.class, args);
+    }
+
+}
---
+++ git_maven/src/test/java/cn/besbing/git_maven/GitMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.besbing.git_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
